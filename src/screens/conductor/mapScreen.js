c8055c941e58de406fc22b29037aeea677894afd
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-07-10 17:28:23 +0530

    map screen 0.1

---
+++ src/screens/conductor/mapScreen.js
@@ -0,0 +1,48 @@
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+
+const initialState = {
+    latitude:null,
+    longitude:null,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+}
+
+const MapScreen = () => {
+
+    const [currentPosition, setCurrentPositiion] = useState(initialState);
+
+   useEffect(()=>{
+    Geolocation.getCurrentPosition(position => {
+      // alert(JSON.stringify(position))
+      const {longitude, latitude} = position.coords;
+      setCurrentPositiion({
+        ...currentPosition,
+        latitude:6.927079,
+        longitude:79.861244,
+      })
+     },error =>alert(error.message),
+     {timeout:20000, maximumAge:1000}
+     )
+   }, [])
+    return currentPosition.latitude ?(
+            <MapView 
+              provider={PROVIDER_GOOGLE}
+              style={{flex:1}}
+              initialRegion={currentPosition}
+              showsUserLocation
+            />
+          ) : <ActivityIndicator style={{flex:1}} animating size="large" /> 
+    }    
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
+
+export default MapScreen;
