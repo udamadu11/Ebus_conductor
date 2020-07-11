91a3946f86d7b320811eecb69702e1b9e171e575
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-07-11 11:32:37 +0530

    login integration

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import client from './client';
 
-const login = (email, password) => client.post('/auth', { email, password });
+const login = (email, password) => client.post('/auth/conductor/login', { email, password });
 
 export default {
     login
