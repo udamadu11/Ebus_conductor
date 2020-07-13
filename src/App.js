b4a5c93b5efe83f010f3a2828a0d2afea43b0a64
Shakthi Sachintha <shakthisachintha@gmail.com>
2020-07-13 08:37:55 +0530

    fix:login error fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import AuthNavigator from './navigations/AuthNavigator';
+import DashboardNavigator from './navigations/DashboardNavigator'
 import navigationTheme from './navigations/navigationTheme';
 
+import AuthContext from './auth/context';
+import authStorage from './auth/storage';
 
-class App extends React.Component {
+const App = () => {
 
-  render() {
-    return (
-      <NavigationContainer theme={navigationTheme}>
-        <AuthNavigator />
-      </NavigationContainer>
-    );
+  const [user, setUser] = useState();
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   }
+
+  useEffect(() => {
+    restoreUser();
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <NavigationContainer theme={navigationTheme}>
+        {user ? <DashboardNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
 };
 
 export default App;
