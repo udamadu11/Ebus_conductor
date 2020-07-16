fa4e6ed807340877896b99c5974b593855241bb2
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-07-16 13:37:52 +0530

    view Passengers

--- src/api/user.js
+++ src/api/user.js
@@ -2,10 +2,11 @@ import client from "./client";
 
 const updateDetails = (userInfo) => client.post("/user/update", userInfo);
 const updatePassword = (userInfo) => client.post("/user/changepassword", userInfo);
-
+const viewPassengers = (userInfo) => client.get("/user/viewpassenger",userInfo);
 
 
 export default {
     updateDetails,
-    updatePassword
+    updatePassword,
+    viewPassengers
 }
